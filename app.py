31a204539e70689e94bde893945b9a4d0e957f6a
flyingman2401 <flyingman24012001@gmail.com>
2023-07-12 01:40:42 +0700

    Modify SSL.

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask
-from flask_sslify import SSLify
+from flask import Flask, redirect, request
 
 app = Flask(__name__)
 
+@app.before_request
+def enforce_ssl():
+    if not request.is_secure:
+        # Thực hiện redirect đến URL mới với giao thức HTTPS
+        url = request.url.replace('http://', 'https://', 1)
+        return redirect(url, code=301)
+
 @app.route('/')
-def hello():
-    return 'Hello, triple D!'
+def home():
+    return 'Đây là trang chủ nhé Triple D!'
 
 if __name__ == '__main__':
     app.run()
-    sslify = SSLify(app)
